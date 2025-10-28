fb5178b3895114aa704d95d04f5b63dd65d414f9
Eliam Araya <eliamaa02@gmail.com>
2025-10-27 21:40:15 -0600

    Creacion de Login

---
+++ public/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login CSGO</title>
+</head>
+<body>
+    <h1><?php echo "Prueba de commit "?> </h1>
+</body>
+</html>
