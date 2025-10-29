bac8638edbce05468f01c83868f77d0f5062c7b0
Pablo Chaves <chavespablo2605@gmail.com>
2025-10-28 21:01:11 -0600

    Creacion de la pagina principal

---
+++ config/start_app.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+date_default_timezone_set("America/Costa_Rica");
+$fecha = new DateTime();
+
+date_default_timezone_set('America/Costa_Rica');
+$hora = date("H:i");
+?>
--- public/index.php
+++ public/index.php
@@ -1,17 +1,49 @@
 <?php
-session_start();
-date_default_timezone_set("America/Costa_Rica");
-$fecha = new DateTime();?>
+require_once '../config/start_app.php'?>;
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>csgo.com</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aventones - Inicio</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/estilos.css">
 </head>
+
 <body>
-    <h1><?php echo "Bienvenido a CSGO, hoy es -> ",  $fecha->format("d-m-Y H:i:s"); ?> </h1>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+    <div class="container">
+      <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="imagenes/logo.png" alt="Logo" class="logo me-2">
+        <span class="fw-bold">Aventones</span>
+      </a>
+
+      <div class="collapse navbar-collapse justify-content-end">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+          <li class="nav-item"><a class="nav-link" href="login.php">Iniciar sesión</a></li>
+          <li class="nav-item"><a class="nav-link" href="registrousuario.php">Registrarse</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
+        </ul>
+      </div>
+
+      <span class="text-light ms-3 hora"><?php echo $hora; ?></span>
+    </div>
+  </nav>
+
+  <header class="hero d-flex flex-column justify-content-center align-items-center text-center text-white">
+    <div class="overlay"></div>
+    <div class="content">
+      <h1 class="fw-bold display-4"><strong>Conduce seguro, Gana oportunidades <strong></h1>
+      <p class="lead mt-3 mb-4">Aventones te conecta con personas que van en tu misma dirección.</p>
+      <a href="login.php" class="btn btn-primary btn-comenzar btn-lg fw-bold">Comenzar</a>
+    </div>
+  </header>
+
+  <footer class="bg-primary text-light text-center py-3">
+   Aventones | Proyecto UTN
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
